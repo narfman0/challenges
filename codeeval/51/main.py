caeeda4e9ed11f8f2bb7341496d966d25ce28940
Jon Robison <narfman0@gmail.com>
2016-04-02 01:39:31 -0400

    -Add solution for 51

--- codeeval/51/main.py
+++ codeeval/51/main.py
@@ -8,13 +8,27 @@ You will be given the x/y co-ordinates of several locations. You will be laying
 out 1 cable between two of these locations. In order to minimise the cost, your
 task is to find the shortest distance between a pair of locations, so that pair
 can be chosen for the cable installation.  """
+import itertools
 import sys
 
 
+def parse_points(lines):
+    """ Parse points out of lines """
+    for line in lines:
+        x, y = line.split(' ')
+        yield (float(x), float(y))
+
+
 def function(lines):
-    import pdb; pdb.set_trace()
-    points = [(float(x), float(y)) for x,y in line.split(' ') for line in lines]
-    return points
+    points = list(parse_points(lines))
+    min_distance_squared = float("inf")
+    for p1, p2 in itertools.combinations(points, 2):
+        x1, y1 = p1
+        x2, y2 = p2
+        distance_squared = (x1 - x2) ** 2 + (y1 - y2) ** 2
+        min_distance_squared = min(min_distance_squared, distance_squared)
+    min_distance = min_distance_squared ** .5
+    return "%.4f" % min_distance if min_distance <= 10000 else "INFINITY"
 
 
 if __name__ == '__main__':
@@ -24,7 +38,10 @@ if __name__ == '__main__':
         lines = f.read().splitlines()
         while lines:
             count = int(lines[0])
-            points = lines[1:count]
-            lines = lines[count+1:]
-            print(str(function(points)))
+            if count:
+                points = lines[1:count+1]
+                lines = lines[count+1:]
+                print(str(function(points)))
+            else:
+                lines = []
     sys.exit(exit_code)
