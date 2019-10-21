18e46cbdf830f746359d518304612e6b290b5219
Jon Robison <narfman0@gmail.com>
2019-10-21 10:29:27 -0400

    -Add lowest common ancestor firs tpass

--- algorithms_java/src/main/java/algs/shared/TreeNode.java
+++ algorithms_java/src/main/java/algs/shared/TreeNode.java
@@ -2,13 +2,18 @@ package algs.shared;
 
 public class TreeNode {
 	public String name;
+	public int val;
 	public TreeNode left, right;
 
 	public TreeNode(String name){
 		this.name = name;
 	}
+
+	public TreeNode(int val){
+		this.val = val;
+	}
 	
 	public String toString(){
-		return name;
+		return name + "," + val;
 	}
 }
