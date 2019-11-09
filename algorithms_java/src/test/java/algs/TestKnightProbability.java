943dbb97ccffc5e517a9f64ccf9b3b66fd6e04b7
Jon Robison <narfman0@gmail.com>
2019-11-09 10:51:34 -0800

    -Swap the actual/expected argument placement

--- algorithms_java/src/test/java/algs/TestKnightProbability.java
+++ algorithms_java/src/test/java/algs/TestKnightProbability.java
@@ -9,11 +9,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestKnightProbability {
     @Test
     public void testKnightProbability3_2_0_0() {
-        assertEquals(knightProbability(3, 2, 0, 0), 0.06250);
+        assertEquals(0.06250, knightProbability(3, 2, 0, 0));
     }
 
     @Test
     public void testKnightProbability8_30_6_4() {
-        assertEquals(knightProbability(8, 30, 6, 4), 0.0001905256629833365);
+        assertEquals(0.0001905256629833365, knightProbability(8, 30, 6, 4));
     }
 }
