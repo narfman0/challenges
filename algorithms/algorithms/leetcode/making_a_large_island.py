de80444beb56723d442f2095ab3c807f3ce3b156
Jon Robison <narfman0@gmail.com>
2024-09-04 10:24:02 -0400

    restructure tests a bit, do not count originating island several times:

--- algorithms/algorithms/leetcode/making_a_large_island.py
+++ algorithms/algorithms/leetcode/making_a_large_island.py
@@ -6,7 +6,7 @@ class Solution:
     def largestIsland(self, grid: List[List[int]]) -> int:
         n = len(grid)
 
-        def calculate_area(x: int, y: int) -> tuple[int, deque]:
+        def calculate_area(x: int, y: int, mark_visited: bool) -> tuple[int, deque]:
             result = 0
             visited = set()
             frontier = deque()
@@ -17,8 +17,10 @@ class Solution:
                 if (x, y) in visited:
                     continue
                 visited.add((x, y))
-                if grid[y][x] == 1:
+                if grid[y][x] in (1, 3):
                     result += 1
+                    if mark_visited:
+                        grid[y][x] == 3
                     if x < n - 1:
                         frontier.append((x + 1, y))
                     if x > 0:
@@ -35,13 +37,13 @@ class Solution:
         for y in range(n):
             for x in range(n):
                 if grid[y][x] == 1:
-                    area, adjacent_water_coords = calculate_area(x, y)
+                    area, adjacent_water_coords = calculate_area(x, y, True)
                     if area == n**2:
                         return area
                     for adjacent_water_coord in adjacent_water_coords:
                         ax, ay = adjacent_water_coord
                         grid[ay][ax] = 1
-                        area, _ = calculate_area(ax, ay)
+                        area, _ = calculate_area(ax, ay, False)
                         max_area = max(max_area, area)
                         grid[ay][ax] = 0
         return max_area
